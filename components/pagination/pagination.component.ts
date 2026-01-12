1d1a8ca5a24ec530cfb47c90b833ee74daf78b28
Anaildo silva <anaildonascimentosilva074@gmail.com>
2026-01-12 08:39:24 -0300

    refactor(pagination): reduzir tamanho do componente extraindo lógica para helpers

--- components/pagination/pagination.component.ts
+++ components/pagination/pagination.component.ts
@@ -23,7 +23,6 @@ import {
   input,
   numberAttribute
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ReplaySubject } from 'rxjs';
 
 import { NzConfigKey, NzConfigService, WithConfig } from 'ng-zorro-antd/core/config';
@@ -33,6 +32,8 @@ import { NzI18nService, NzPaginationI18nInterface } from 'ng-zorro-antd/i18n';
 import { NzPaginationDefaultComponent } from './pagination-default.component';
 import { NzPaginationSimpleComponent } from './pagination-simple.component';
 import { PaginationItemRenderContext, type NzPaginationAlign } from './pagination.types';
+import { validatePageIndex, getLastIndex } from './pagination-utils.helper';
+import { setupPaginationSubscriptions } from './pagination-subscription.helper';
 
 const NZ_CONFIG_MODULE_NAME: NzConfigKey = 'pagination';
 
@@ -41,42 +42,7 @@ const NZ_CONFIG_MODULE_NAME: NzConfigKey = 'pagination';
   exportAs: 'nzPagination',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-    @if (showPagination) {
-      @if (nzSimple) {
-        <ng-template [ngTemplateOutlet]="simplePagination.template" />
-      } @else {
-        <ng-template [ngTemplateOutlet]="defaultPagination.template" />
-      }
-    }
-
-    <nz-pagination-simple
-      #simplePagination
-      [disabled]="nzDisabled"
-      [itemRender]="nzItemRender"
-      [locale]="locale"
-      [pageSize]="nzPageSize"
-      [total]="nzTotal"
-      [pageIndex]="nzPageIndex"
-      (pageIndexChange)="onPageIndexChange($event)"
-    />
-    <nz-pagination-default
-      #defaultPagination
-      [nzSize]="size"
-      [itemRender]="nzItemRender"
-      [showTotal]="nzShowTotal"
-      [disabled]="nzDisabled"
-      [locale]="locale"
-      [showSizeChanger]="nzShowSizeChanger"
-      [showQuickJumper]="nzShowQuickJumper"
-      [total]="nzTotal"
-      [pageIndex]="nzPageIndex"
-      [pageSize]="nzPageSize"
-      [pageSizeOptions]="nzPageSizeOptions"
-      (pageIndexChange)="onPageIndexChange($event)"
-      (pageSizeChange)="onPageSizeChange($event)"
-    />
-  `,
+  templateUrl: './pagination.component.html',
   host: {
     class: 'ant-pagination',
     '[class.ant-pagination-simple]': 'nzSimple',
@@ -123,87 +89,51 @@ export class NzPaginationComponent implements OnInit, OnChanges {
 
   private total$ = new ReplaySubject<number>(1);
 
-  validatePageIndex(value: number, lastIndex: number): number {
-    if (value > lastIndex) {
-      return lastIndex;
-    } else if (value < 1) {
-      return 1;
-    } else {
-      return value;
-    }
-  }
-
-  onPageIndexChange(index: number): void {
-    const lastIndex = this.getLastIndex(this.nzTotal, this.nzPageSize);
-    const validIndex = this.validatePageIndex(index, lastIndex);
+  onPageIndexChange = (index: number): void => {
+    const lastIndex = getLastIndex(this.nzTotal, this.nzPageSize);
+    const validIndex = validatePageIndex(index, lastIndex);
     if (validIndex !== this.nzPageIndex && !this.nzDisabled) {
       this.nzPageIndex = validIndex;
       this.nzPageIndexChange.emit(this.nzPageIndex);
     }
-  }
-
-  onPageSizeChange(size: number): void {
+  };
+  onPageSizeChange = (size: number): void => {
     this.nzPageSize = size;
     this.nzPageSizeChange.emit(size);
-    const lastIndex = this.getLastIndex(this.nzTotal, this.nzPageSize);
-    if (this.nzPageIndex > lastIndex) {
-      this.onPageIndexChange(lastIndex);
-    }
-  }
-
-  onTotalChange(total: number): void {
-    const lastIndex = this.getLastIndex(total, this.nzPageSize);
+    const lastIndex = getLastIndex(this.nzTotal, this.nzPageSize);
+    if (this.nzPageIndex > lastIndex) this.onPageIndexChange(lastIndex);
+  };
+  onTotalChange = (total: number): void => {
+    const lastIndex = getLastIndex(total, this.nzPageSize);
     if (this.nzPageIndex > lastIndex) {
       Promise.resolve().then(() => {
         this.onPageIndexChange(lastIndex);
         this.cdr.markForCheck();
       });
     }
-  }
+  };
 
-  getLastIndex(total: number, pageSize: number): number {
-    return Math.ceil(total / pageSize);
-  }
+  ngOnInit = (): void => {
+    setupPaginationSubscriptions(
+      this.i18n,
+      this.breakpointService,
+      this.directionality,
+      this.destroyRef,
+      this.cdr,
+      this.total$,
+      this.nzResponsive,
+      (locale) => this.locale = locale,
+      (total) => this.onTotalChange(total),
+      (size) => this.size = size,
+      (dir) => this.dir = dir
+    );
+  };
 
-  ngOnInit(): void {
-    this.i18n.localeChange.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
-      this.locale = this.i18n.getLocaleData('Pagination');
-      this.cdr.markForCheck();
-    });
-
-    this.total$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(total => {
-      this.onTotalChange(total);
-    });
-
-    this.breakpointService
-      .subscribe(gridResponsiveMap)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(bp => {
-        if (this.nzResponsive) {
-          this.size = bp === NzBreakpointEnum.xs ? 'small' : 'default';
-          this.cdr.markForCheck();
-        }
-      });
-
-    this.directionality.change?.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(direction => {
-      this.dir = direction;
-      this.cdr.detectChanges();
-    });
-    this.dir = this.directionality.value;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const { nzHideOnSinglePage, nzTotal, nzPageSize, nzSize } = changes;
-    if (nzTotal) {
-      this.total$.next(this.nzTotal);
+  ngOnChanges = (changes: SimpleChanges): void => {
+    if (changes['nzTotal']) this.total$.next(this.nzTotal);
+    if (changes['nzHideOnSinglePage'] || changes['nzTotal'] || changes['nzPageSize']) {
+      this.showPagination = (this.nzHideOnSinglePage && this.nzTotal > this.nzPageSize) || (this.nzTotal > 0 && !this.nzHideOnSinglePage);
     }
-    if (nzHideOnSinglePage || nzTotal || nzPageSize) {
-      this.showPagination =
-        (this.nzHideOnSinglePage && this.nzTotal > this.nzPageSize) || (this.nzTotal > 0 && !this.nzHideOnSinglePage);
-    }
-
-    if (nzSize) {
-      this.size = nzSize.currentValue;
-    }
-  }
+    if (changes['nzSize']) this.size = changes['nzSize'].currentValue;
+  };
 }
